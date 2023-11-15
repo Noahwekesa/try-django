a92b95c2e9b3d706007fb0696a1464440645af10
noah wekesa <naohwekesa0@gmail.com>
2023-11-15 18:35:15 +0300

    learn

--- src/products/models.py
+++ src/products/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=100) # max_length always required when using charfield
     description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(decimal_places=2, max_digits=1000)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     summary = models.TextField(blank=False, null=True)
     featured = models.BooleanField(default=True)
--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -19,12 +19,13 @@ from django.urls import path
 #pages
 from pages.views import home_view, contact_view, about_view, social_view
 #product
-from products.views import product_detail_view
+from products.views import product_detail_view, product_create_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
     path('contact/', contact_view, name='contact'),
     path('about/', about_view, name='about'),
     path('social/', social_view, name='social'),
-    path('product/', product_detail_view, name='')
+    path('product/', product_detail_view, name='product-detail'),
+    path('create/', product_create_view, name='product-create')
 ]
