fa605185127587eefd7455671e76236eee9f7c1e
noahwekesa <noahwekesa0@gmail.com>
2024-08-19 18:07:09 +0300

    cleared repo to start the try-django3.2

--- src/products/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Product(models.Model):
-    title = models.CharField(max_length=100) # max_length always required when using charfield
-    description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    summary = models.TextField(blank=False, null=True)
-    featured = models.BooleanField(default=True)
